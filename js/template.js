ddfabc4701b3e6deaee08736cc9dc14b9a965deb
wangerbing <erbing22@126.com>
2015-04-15 17:55:04 +0800

    修改格式

--- js/template.js
+++ js/template.js
@@ -7,38 +7,40 @@
  * File Description: Initializations of plugins 
  */
 
-(function($){
-	$(document).ready(function(){
-	
+(function ($) {
+	$(document).ready(function () {
+
 		$(".banner-image").backstretch('images/banner.jpg');
-		
+
 		// Fixed header
 		//-----------------------------------------------
-		$(window).scroll(function() {
-			if (($(".header.fixed").length > 0)) { 
-				if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+		$(window).scroll(function () {
+			if (($(".header.fixed").length > 0)) {
+				if (($(this).scrollTop() > 0) && ($(window).width() > 767)) {
 					$("body").addClass("fixed-header-on");
 				} else {
 					$("body").removeClass("fixed-header-on");
 				}
-			};
+			}
+			;
 		});
 
-		$(window).load(function() {
-			if (($(".header.fixed").length > 0)) { 
-				if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+		$(window).load(function () {
+			if (($(".header.fixed").length > 0)) {
+				if (($(this).scrollTop() > 0) && ($(window).width() > 767)) {
 					$("body").addClass("fixed-header-on");
 				} else {
 					$("body").removeClass("fixed-header-on");
 				}
-			};
+			}
+			;
 		});
 
 		//Scroll Spy
 		//-----------------------------------------------
-		if($(".scrollspy").length>0) {
+		if ($(".scrollspy").length > 0) {
 			$("body").addClass("scroll-spy");
-			$('body').scrollspy({ 
+			$('body').scrollspy({
 				target: '.scrollspy',
 				offset: 152
 			});
@@ -46,14 +48,14 @@
 
 		//Smooth Scroll
 		//-----------------------------------------------
-		if ($(".smooth-scroll").length>0) {
-			$('.smooth-scroll a[href*=#]:not([href=#]), a[href*=#]:not([href=#]).smooth-scroll').click(function() {
-				if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		if ($(".smooth-scroll").length > 0) {
+			$('.smooth-scroll a[href*=#]:not([href=#]), a[href*=#]:not([href=#]).smooth-scroll').click(function () {
+				if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 					var target = $(this.hash);
-					target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+					target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 					if (target.length) {
 						$('html,body').animate({
-							scrollTop: target.offset().top-151
+							scrollTop: target.offset().top - 151
 						}, 1000);
 						return false;
 					}
@@ -63,13 +65,13 @@
 
 		// Animations
 		//-----------------------------------------------
-		if (($("[data-animation-effect]").length>0) && !Modernizr.touch) {
-			$("[data-animation-effect]").each(function() {
+		if (($("[data-animation-effect]").length > 0) && !Modernizr.touch) {
+			$("[data-animation-effect]").each(function () {
 				var $this = $(this),
-				animationEffect = $this.attr("data-animation-effect");
-				if(Modernizr.mq('only all and (min-width: 768px)') && Modernizr.csstransitions) {
-					$this.appear(function() {
-						setTimeout(function() {
+					animationEffect = $this.attr("data-animation-effect");
+				if (Modernizr.mq('only all and (min-width: 768px)') && Modernizr.csstransitions) {
+					$this.appear(function () {
+						setTimeout(function () {
 							$this.addClass('animated object-visible ' + animationEffect);
 						}, 400);
 					}, {accX: 0, accY: -130});
@@ -77,12 +79,13 @@
 					$this.addClass('object-visible');
 				}
 			});
-		};
+		}
+		;
 
 		// Isotope filters
 		//-----------------------------------------------
-		if ($('.isotope-container').length>0) {
-			$(window).load(function() {
+		if ($('.isotope-container').length > 0) {
+			$(window).load(function () {
 				$('.isotope-container').fadeIn();
 				var $container = $('.isotope-container').isotope({
 					itemSelector: '.isotope-item',
@@ -91,21 +94,22 @@
 					filter: "*"
 				});
 				// filter items on button click
-				$('.filters').on( 'click', 'ul.nav li a', function() {
+				$('.filters').on('click', 'ul.nav li a', function () {
 					var filterValue = $(this).attr('data-filter');
 					$(".filters").find("li.active").removeClass("active");
 					$(this).parent().addClass("active");
-					$container.isotope({ filter: filterValue });
+					$container.isotope({filter: filterValue});
 					return false;
 				});
 			});
-		};
+		}
+		;
 
 		//Modal
 		//-----------------------------------------------
-		if($(".modal").length>0) {
-			$(".modal").each(function() {
-				$(".modal").prependTo( "body" );
+		if ($(".modal").length > 0) {
+			$(".modal").each(function () {
+				$(".modal").prependTo("body");
 			});
 		}
 
